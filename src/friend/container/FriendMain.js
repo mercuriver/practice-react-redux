a6bc6c4c1718fdeee674749b783e56aef9c5836d
강수성 <mercuriver@gmail.com>
2020-11-29 18:12:41 +0900

    feat(friend): Filter friend list

--- src/friend/container/FriendMain.js
+++ src/friend/container/FriendMain.js
@@ -1,38 +1,78 @@
 import React from "react";
-import store from "../../common/store";
+import { connect } from "react-redux";
+
 import { getNextFriend } from "../../common/mockData";
-import { addFriend } from "../state";
+import * as actions from "../state";
+
 import FriendList from "../component/FriendList";
+import NumberSelect from "../component/NumberSelect";
+import { MAX_AGE_LIMIT, MAX_SHOW_LIMIT } from "../common";
 
-class FriendMain extends React.PureComponent {
-  state = {
-    friends: store.getState().friend.friends,
-  };
+const ageLimitOptions = [
+  15,
+  20,
+  25,
+  30,
+  35,
+  40,
+  45,
+  50,
+  55,
+  60,
+  65,
+  70,
+  MAX_AGE_LIMIT,
+];
+const showLimitOptions = [2, 4, 6, MAX_SHOW_LIMIT];
 
-  componentDidMount() {
-    this.unsubscribe = store.subscribe(() => {
-      this.setState({ friends: store.getState().friend.friends });
-    });
-  }
-
-  componentWillUnmount() {
-    this.unsubscribe();
-  }
-
+class FriendMain extends React.Component {
   onAdd = () => {
     const friend = getNextFriend();
-    store.dispatch(addFriend(friend));
+    this.props.addFriend(friend);
   };
 
   render() {
     console.log("FriendMain render");
+    const {
+      friendsWithAgeLimit,
+      friendsWithAgeShowLimit,
+      ageLimit,
+      showLimit,
+      setAgeLimit,
+      setShowLimit,
+    } = this.props;
+
     return (
       <div>
         <button onClick={this.onAdd}>친구 추가</button>
-        <FriendList friends={this.state.friends} />
+        <NumberSelect
+          onChange={setAgeLimit}
+          value={ageLimit}
+          options={ageLimitOptions}
+          postfix="세 이하만 보기"
+        />
+        <FriendList friends={friendsWithAgeLimit} />
+        <NumberSelect
+          onChange={setShowLimit}
+          value={showLimit}
+          options={showLimitOptions}
+          postfix="명 이하만 보기(연령 제한 적용)"
+        />
+        <FriendList friends={friendsWithAgeShowLimit} />
       </div>
     );
   }
 }
 
-export default FriendMain;
+const mapStateToProps = (state) => {
+  const { friends, ageLimit, showLimit } = state.friend;
+
+  const friendsWithAgeLimit = friends.filter(
+    (friend) => friend.age <= ageLimit
+  );
+  const friendsWithAgeShowLimit = friendsWithAgeLimit.slice(0, showLimit);
+
+  return { friendsWithAgeLimit, friendsWithAgeShowLimit, friends, ageLimit };
+};
+
+export default connect(mapStateToProps, actions)(FriendMain);
--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+
+import store from "./common/store";
+
 import TimeLineMain from "./timeline/container/TimeLineMain";
 import FriendMain from "./friend/container/FriendMain";
 
 ReactDOM.render(
-  <div>
-    <FriendMain />
-    <TimeLineMain />
-  </div>,
+  <Provider store={store}>
+    <div>
+      <FriendMain />
+      <TimeLineMain />
+    </div>
+  </Provider>,
   document.getElementById("root")
 );
